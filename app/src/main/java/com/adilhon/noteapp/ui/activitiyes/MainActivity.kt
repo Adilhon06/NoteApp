523aefa05c93a0a3ec1861e1cc21ffe21f5903b2
Adilhon06 <adubaichik2006@gmail.com>
2024-06-01 16:16:07 +0600

    Initial commit

---
+++ app/src/main/java/com/adilhon/noteapp/ui/activitiyes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adilhon.noteapp.ui.activitiyes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adilhon.noteapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
